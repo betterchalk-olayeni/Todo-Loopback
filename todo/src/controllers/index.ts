13ca02ae2e7e4e00ce68a236e937b52034bdf4e1
betterchalk-olayeni <olayeni@betterchalk.com>
2021-07-29 09:21:22 +0000

    Added TodoList Controllers

--- todo/src/controllers/index.ts
+++ todo/src/controllers/index.ts
@@ -1,4 +1,6 @@
 export * from './ping.controller';
 export * from './todo.controller';
 export * from './todo-list-todo.controller';
-export * from './todo-todo-list.controller';
+// export * from './todo-todo-list.controller'; This line was commented because the file was deleted after
+//moving its content to todo.controller.ts
+export * from './todo-list.controller';
--- todo/src/controllers/todo.controller.ts
+++ todo/src/controllers/todo.controller.ts
@@ -17,7 +17,7 @@ import {
   requestBody,
   response,
 } from '@loopback/rest';
-import {Todo} from '../models';
+import {Todo, TodoList} from '../models';
 import {TodoRepository} from '../repositories';
 
 export class TodoController {
@@ -58,14 +58,17 @@ export class TodoController {
     return this.todoRepository.count(where);
   }
 
-  @get('/todos')
-  @response(200, {
-    description: 'Array of Todo model instances',
-    content: {
-      'application/json': {
-        schema: {
-          type: 'array',
-          items: getModelSchemaRef(Todo, {includeRelations: true}),
+  @get('/todos', {
+    responses: {
+      '200': {
+        description: 'Array of Todo model instances',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: getModelSchemaRef(Todo, {includeRelations: true}),
+            },
+          },
         },
       },
     },
@@ -95,12 +98,15 @@ export class TodoController {
     return this.todoRepository.updateAll(todo, where);
   }
 
-  @get('/todos/{id}')
-  @response(200, {
-    description: 'Todo model instance',
-    content: {
-      'application/json': {
-        schema: getModelSchemaRef(Todo, {includeRelations: true}),
+  @get('/todos/{id}', {
+    responses: {
+      '200': {
+        description: 'Todo model instance',
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(Todo, {includeRelations: true}),
+          },
+        },
       },
     },
   })
@@ -147,4 +153,24 @@ export class TodoController {
   async deleteById(@param.path.number('id') id: number): Promise<void> {
     await this.todoRepository.deleteById(id);
   }
+
+  //Controller from todo-todo-list.controller.ts
+  @get('/todos/{id}/todo-list', {
+    responses: {
+      '200': {
+        description: 'TodoList belonging to Todo',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(TodoList)},
+          },
+        },
+      },
+    },
+  })
+  async getTodoList(
+    @param.path.number('id') id: typeof Todo.prototype.id,
+  ): Promise<TodoList> {
+    return this.todoRepository.todoList(id);
+  }
+
 }
