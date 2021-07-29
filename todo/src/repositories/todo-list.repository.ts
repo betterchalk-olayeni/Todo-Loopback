36594dc4056dfbfb66ae59471ee009d9014d04fc
betterchalk-olayeni <olayeni@betterchalk.com>
2021-07-29 09:02:08 +0000

    Added TodoList Repository

---
+++ todo/src/repositories/todo-list.repository.ts
@@ -0,0 +1,22 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {DbDataSource} from '../datasources';
+import {TodoList, TodoListRelations} from '../models';
+
+export class TodoListRepository extends DefaultCrudRepository<
+  TodoList,
+  typeof TodoList.prototype.id,
+  TodoListRelations
+> {
+  constructor(
+    @inject('datasources.db') dataSource: DbDataSource,
+  ) {
+    super(TodoList, dataSource);
+  }
+
+   //Finding a Todo-List with a specific title
+   public findByTitle(title: string) {
+    return this.findOne({where: {title}});
+  }
+
+}
